4a79c0edac6606cad85ab7598f37a27a865f7d33
Anurag <139263850+Anurag-nsg@users.noreply.github.com>
2024-12-10 02:09:05 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -58,11 +58,11 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Login user1={setLog} email={setMail}/>} />
         <Route path="*" element={auth.currentUser && mail!==''  ? <PageNotFound /> :<Login user1={setLog} email={setMail} />} />
-        <Route path="/home" element={<Home mail={mail} />} />
-        <Route path="/foam" element={<Psinfo mail={mail} />} />
-        <Route path="/chatbot" element={<ChatBot mail={mail} />} />
-        <Route path='/fileupload' element={<FileUpload/>} />
-        <Route path='/personal-MF' element={<InvestmentRecommendationForm/>} />
+        <Route path="/home" element={auth.currentUser && mail!==''  ?<Home mail={mail} />:<Login user1={setLog} email={setMail} />}} />
+        <Route path="/foam" element={auth.currentUser && mail!==''  ?<Psinfo mail={mail} />:<Login user1={setLog} email={setMail} />}} />
+        <Route path="/chatbot" element={auth.currentUser && mail!==''  ?<ChatBot mail={mail} />:<Login user1={setLog} email={setMail} />}} />
+        <Route path='/fileupload' element={auth.currentUser && mail!==''  ?<FileUpload/>:<PageNotFound />}} />
+        <Route path='/personal-MF' element={auth.currentUser && mail!==''  ?<InvestmentRecommendationForm/>:<Login user1={setLog} email={setMail} />}} />
 
       </Routes>
     </div>
