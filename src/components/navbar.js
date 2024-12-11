264f3a6026616261373d66242adbb279af9ea795
Anurag <139263850+Anurag-nsg@users.noreply.github.com>
2024-12-11 22:28:55 +0530

    Update navbar.js

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -45,11 +45,11 @@ const Navbar = (user) => {
 
   return (
     <div className=" navbar navbar-expand-lg navbar-dark  bg-gradient-to-br from-blue-900/90 to-purple-900/80  text-white scale-15 shadow-lg">
-      <div className="container-fluid flex p-3"  onClick={() => {navigate('/home')}}>
+      <div className="container-fluid flex p-3" >
         <a className="navbar-brand" onClick={() => {navigate('/home')}} style={{cursor: 'pointer'}}>
           <img src="./navlogo1.png" style={{ maxHeight: '70px', width: '50px', maxWidth: '100%', height: 'auto', marginLeft: '5px' ,background:'transparent',borderRadius:'10px',padding:'4px',boxShadow: 'rgba(9, 30, 66, 0.25) 0px 4px 8px -2px, rgba(9, 30, 66, 0.08) 0px 0px 0px 1px',}} alt="Navbar Logo" />
         </a>
-        <h1 style={{ fontWeight: 'bolder',marginTop:'10px',marginLeft:'4px' }}>WealthWise</h1>
+        <h1 style={{ fontWeight: 'bolder',marginTop:'10px',marginLeft:'4px' }}  onClick={() => {navigate('/home')}}>WealthWise</h1>
         </div>
         </div>
   );
