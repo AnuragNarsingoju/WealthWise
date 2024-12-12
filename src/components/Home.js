98a9d61bee0e5222873c5e943692ca41921860d8
Anurag <139263850+Anurag-nsg@users.noreply.github.com>
2024-12-13 02:57:55 +0530

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -180,14 +180,11 @@ const Home = ({ mail }) => {
             },
             { 
               name: 'Expense Tracker',
-              icon: (
-                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 24 24" fill="currentColor">
-                  <path d="M21 6a2 2 0 00-2-2H5a2 2 0 00-2 2v9a2 2 0 002 2h7l4 4v-4h3a2 2 0 002-2V6z" />
-                  <circle cx="8" cy="10" r="1.5" />
-                  <circle cx="12" cy="10" r="1.5" />
-                  <circle cx="16" cy="10" r="1.5" />
+             icon: (
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 512 512" fill="white">
+                    <path d="M64 64c0-17.7-14.3-32-32-32S0 46.3 0 64L0 400c0 44.2 35.8 80 80 80l400 0c17.7 0 32-14.3 32-32s-14.3-32-32-32L80 416c-8.8 0-16-7.2-16-16L64 64zm406.6 86.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L320 210.7l-57.4-57.4c-12.5-12.5-32.8-12.5-45.3 0l-112 112c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L240 221.3l57.4 57.4c12.5 12.5 32.8 12.5 45.3 0l128-128z"/>
                 </svg>
-            )
+              )
             }
           ];
         const investments = [
