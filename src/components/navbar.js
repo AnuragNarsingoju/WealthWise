ce68d3ce038d43480e5f41212ceadd4bc7ef46a1
Anurag <139263850+Anurag-nsg@users.noreply.github.com>
2024-12-13 01:28:42 +0530

    Update navbar.js

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -86,8 +86,8 @@ const Navbar = ({mail}) => {
           <path fillRule="evenodd" d="M18 5v8a2 2 0 01-2 2h-5l-5 4v-4H4a2 2 0 01-2-2V5a2 2 0 012-2h12a2 2 0 012 2zM7 8H5v2h2V8zm2 0h2v2H9V8zm6 0h-2v2h2V8z" clipRule="evenodd" />
         </svg>
       ),
-      label: 'ChatBot',
-      key: 'ChatBot'
+      label: 'Chat With Niveshak',
+      key: 'Chat With Niveshak'
     },
     { 
       icon: <Activity className="w-5 h-5" />, 
@@ -137,7 +137,7 @@ const Navbar = ({mail}) => {
                     if (item.key === 'home') {
                      navigate('/home',{ replace: true });
                     }
-                    if (item.key === 'ChatBot') {
+                    if (item.key === 'Chat With Niveshak') {
                       navigate('/chatbot',{ replace: true });
                      }
                   }}
@@ -308,6 +308,12 @@ const Navbar = ({mail}) => {
                   key={item.key}
                   onClick={() => {
                     setActiveTab(item.key);
+                    if (item.key === 'home') {
+                     navigate('/home',{ replace: true });
+                    }
+                    if (item.key === 'Chat With Niveshak') {
+                      navigate('/chatbot',{ replace: true });
+                     }
                     setIsMobileMenuOpen(false);
                   }}
                   className={`
