81e046af04dc3651595d202704de421bfc5c3e95
Anurag <139263850+Anurag-nsg@users.noreply.github.com>
2024-12-14 01:51:27 +0530

    Update ExpenseTracker.js

--- src/components/ExpenseTracker.js
+++ src/components/ExpenseTracker.js
@@ -2,8 +2,13 @@ import react ,{useState,useEffect} from "react";
 import Navbar from "./navbar";
 import ExpenseComparison from "./ExpenseComparison";
 import BudgetPieChart from "./BudgetPieChart";
+import { useLocation } from 'react-router-dom';
+
 
 const ExpenseTracker = () => {
+    const location = useLocation();
+    const { data, mail } = location.state || {}; 
+
     return ( 
         <>
         <div className="relative bg-gradient-to-br 
@@ -23,8 +28,8 @@ const ExpenseTracker = () => {
             justify-center
             "
         >
-            <ExpenseComparison/>
-            <BudgetPieChart/>
+            <ExpenseComparison data={data} />
+            <BudgetPieChart data={data}/>
         </div>
        
         
