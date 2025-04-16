f2ae53475637c7888c72a0e3d649735daa30338f
Anurag <139263850+AnuragNarsingoju@users.noreply.github.com>
2025-04-16 16:40:12 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ import PersonalFDRecommendation from './components/personalFD';
 import ExpenseDate from './components/ExpenseDate';
 import ExpenseTracker from './components/ExpenseTracker';
 import PersonalizedStocks from './components/PersonalizedStocks';
-import Portfolio from './components/Portfolio';\
+import Portfolio from './components/Portfolio';
 
 
 const App = () => {
