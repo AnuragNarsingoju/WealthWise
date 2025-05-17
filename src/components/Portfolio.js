e4c9b031fa4472f7ff445d60cdfcef9a690fde99
Anurag <139263850+AnuragNarsingoju@users.noreply.github.com>
2025-05-17 12:47:19 +0530

    Update Portfolio.js

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -25,7 +25,7 @@ const Portfolio = () => {
   const fetchPortfolioData = async () => {
     setLoading(true);
     try {
-      const response = await axios.post(
+      const response = await axios.get(
         `${process.env.REACT_APP_BACKEND_URL}portfolio?email=${emailId}`
       );
       const data = response.data;
